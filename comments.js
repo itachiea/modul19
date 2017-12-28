371636c3b6f0c2ea62e7c2d58f4ccb58e5c750ab
ichi <w.konrad60@gmail.com>
2017-12-28 01:28:42 +0100

    poprawki

--- comments.js
+++ comments.js
@@ -1,49 +1,49 @@
-import {ADD_COMMENT} from './actions';
-import {REMOVE_COMMENT} from './actions';
-import {EDIT_COMMENT} from './actions';
-import {THUMB_UP_COMMENT} from './actions';
-import {THUMB_DOWN_COMMENT} from './actions';
-
-const initialState = {
-	comments: [],
-	users: []
-}
+import {
+	ADD_COMMENT,
+	REMOVE_COMMENT,
+	EDIT_COMMENT,
+	THUMB_UP_COMMENT,
+	THUMB_DOWN_COMMENT
+} from './actions';
+import uuid from 'uuid/v4';
 
 function comments(state = [], action) {
 	switch(action.type) {
 		case ADD_COMMENT:
-			return Object.assign({}, state, {
-				comments: [{
-					id: action.id,
+			return [
+				...state,
+				{
+					id: uuid(),
 					text: action.text,
 					votes: 0
-				}, ...state.comments]
-			});
+				}
+			];			
 		case REMOVE_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.filter(comment => comment.id !== action.id)
-			});
+			return state.filter(comment => comment.id !== action.id);
 		case EDIT_COMMENT:
-			return Object.assign({}, state, {
-				comments: [{
-					id: action.id,
-					text: action.text
-				}, ...state.comments]
+			return state.map(comment => {
+				if (comment.id === action.id ) {
+					comment.text = action.text
+				}
+
+				return comment;
 			});
 		case THUMB_UP_COMMENT:
-			return Object.assign({}, state, {
-				comments: [{
-					id: action.id,
-					votes: +1
-				}, ...state.comments]
+			return state.map(comment => {
+				if (comment.id === action.id) {
+					comment.votes = comment.votes + 1;
+				}
+
+				return comment;
 			});
 		case THUMB_DOWN_COMMENT:
-			return Object.assign({}, state, {
-				comments: [{
-					id: action.id,
-					votes: -1
-				}, ...state.comments]
-			});
+		return state.map(comment => {
+			if (comment.id === action.id) {
+				comment.votes = comment.votes - 1;
+			}
+
+			return comment;
+		});
 		default:
 			return state;
 	}
